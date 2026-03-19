f285ebf370e7736b5c85c9b85af3d504a59a56f3
namtrungle20 <121472249+namtrungle20@users.noreply.github.com>
2026-03-19 16:30:05 +0700

    feat: update lại sanpham.router.js

--- src/routes/sanpham.routes.js
+++ src/routes/sanpham.routes.js
@@ -3,7 +3,8 @@ import asyncHandler from '../middlewares/asyncHandler.js';
 import validate from '../middlewares/validate.middleware.js';
 import { requestVaiTro } from '../middlewares/auth.middleware.js';
 import { uploadSingle } from '../middlewares/upload.middleware.js';
-import * as SanPhamController from '../controller/SanPham.controller.js';
+import { getSanPhams, getSanPhamById, themSanPham, updateSanPham, xoaSanPham } from '../controller/SanPham.controller.js'
+
 import ThemSanPhamRequest from '../dtos/requests/SanPham/ThemSanPhamRequest.js';
 import UpdateSanPhamRequest from '../dtos/requests/SanPham/UpdateSanPhamRequest.js';
 import { VaiTroNguoiDung } from '../constants/index.js';
@@ -11,10 +12,10 @@ import { VaiTroNguoiDung } from '../constants/index.js';
 const router = Router();
 const adminOnly = requestVaiTro([VaiTroNguoiDung.ADMIN]);
 
-router.get('/', asyncHandler(SanPhamController.getSanPhams));
-router.get('/:id', asyncHandler(SanPhamController.getSanPhamsById));
-router.post('/', adminOnly, uploadSingle, validate(ThemSanPhamRequest), asyncHandler(SanPhamController.themSanPhams));
-router.put('/:id', adminOnly, uploadSingle, validate(UpdateSanPhamRequest), asyncHandler(SanPhamController.updateSanPhams));
-router.delete('/:id', adminOnly, asyncHandler(SanPhamController.xoaSanPhams));
+router.get('/', asyncHandler(getSanPhams))
+router.get('/:id', asyncHandler(getSanPhamById))
+router.post('/', adminOnly, uploadSingle, validate(ThemSanPhamRequest), asyncHandler(themSanPham))
+router.put('/:id', adminOnly, uploadSingle, validate(UpdateSanPhamRequest), asyncHandler(updateSanPham))
+router.delete('/:id', adminOnly, asyncHandler(xoaSanPham))
 
 export default router;
